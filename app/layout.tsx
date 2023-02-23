b8432d85a24dd64b8bf920ec03b373cff7b5ff5b
Katharina Simma <katharina.simma@diebin.at>
2023-02-23 18:55:42 +0100

    Add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,28 @@
-import './globals.css'
+import './globals.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export const metadata = {
+  title: {
+    default: 'Taskology',
+    template: '%s | Taskology',
+  },
+  icons: {
+    shortcut: '/favicon.ico',
+  },
+};
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout(props: Props) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <header>TASKOLOGY</header>
+        {props.children}
+        <footer>Created by Katharina Simma with love</footer>
+      </body>
     </html>
-  )
+  );
 }
