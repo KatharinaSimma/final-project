d1a120a366500cbbfb7c0d904a31812accddb4ab
Katharina Simma <katharina.simma@diebin.at>
2023-03-12 17:57:58 +0100

    Add authorization logic to homePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,31 @@
 import { gql } from '@apollo/client';
-// import { cookies } from 'next/headers';
-// import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import { initializeApollo } from '../util/graphql';
 import ApolloClientProvider from './ApolloClientProvider';
 import ListContainer from './ListContainer';
 
-export default async function Home() {
+export default async function HomePage() {
   const client = initializeApollo(null);
+  const sessionToken = cookies().get('sessionToken');
 
-  // const nextCookies = cookies();
-
-  // const fakeSessionToken = nextCookies.get('fakeSessionToken');
-  // const { data } = await client.query({
-  //   query: gql`
-  //     query GetLoggedInAnimalByFirstName($firstName: String! = ${fakeSessionToken?.value}) {
-  //       getLoggedInAnimalByFirstName(firstName: $firstName) {
-  //         accessory
-  //         firstName
-  //       }
-  //     }
-  //   `,
-  // });
-
-  // if (!data.getLoggedInAnimalByFirstName) {
-  //   redirect('/login');
-  // }
-
-  // await client.query({
-  //   query: gql`
-  //     query ListWithTasks {
-  //       listWithTasks {
-  //         id
-  //         title
-  //         description
-  //         tasks {
-  //           id
-  //           title
-  //           description
-  //           done
-  //         }
-  //       }
-  //     }
-  //   `,
-  // });
-
-  await client.query({
+  const { data, loading } = await client.query({
     query: gql`
-      query User {
-        listWithTasks {
-          id
-          title
-          description
-          tasks {
-            id
-            title
-            description
-            done
-          }
-        }
+    query userBySessionToken($token: String! = "${sessionToken?.value}") {
+      userBySessionToken(token: $token) {
+        id
+        username
       }
-    `,
+    }
+  `,
   });
+
+  if (loading) return <button className="btn loading">loading</button>;
+
+  if (!data.userBySessionToken) {
+    redirect('/login');
+  }
+
   return (
     <main className="min-h-[calc(100vh-7rem)] mb-12 p-2">
       <h1 className="py-5 text-3xl text-center">Your Lists</h1>
