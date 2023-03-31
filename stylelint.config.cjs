c988d62eaf3bf0d6d9c3d7def7136b88870aaea2
Katharina Simma <katharina.simma@diebin.at>
2023-03-31 16:40:23 +0200

    Fix stylint again

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -4,11 +4,14 @@ const config = {
     'stylelint-config-recommended',
     'stylelint-config-recommended-scss',
     'stylelint-config-css-modules',
+    'stylelint-scss',
   ],
   rules: {
     'no-descending-specificity': null,
     // Allow files without any styles
     'no-empty-source': null,
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
   },
   unknownAtRules: 'ignore',
   overrides: [
